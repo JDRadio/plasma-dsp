16076339cceac20c51783e7fb568a3793a58cb3a
Jean-Sébastien Dominique <jd@amateurradio.engineer>
2020-02-03 23:12:58 -0500

    Moved audio frontend code into its own class

--- src/main.cpp
+++ src/main.cpp
@@ -5,136 +5,160 @@
 #include <array>
 #include <cstdint>
 #include <cstddef>
+#include <cmath>
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <algorithm>
 
 using namespace std;
 
-int main(int argc, char* argv[])
+class AudioFrontend
 {
-    list<ALuint> bufferQueue;
+public:
+    AudioFrontend(void);
+    ~AudioFrontend(void);
 
-    ALenum errorCode = 0;
-    array<ALuint, 16> helloBuffer;
-    ALuint helloSource;
+    size_t receive(float* in, size_t in_size);
+    void transmit(const float* out, size_t out_size);
 
-    auto szDefaultCaptureDevice = alcGetString(NULL, ALC_CAPTURE_DEFAULT_DEVICE_SPECIFIER);
-    cout << "Default Capture Device: " << szDefaultCaptureDevice << endl;
+protected:
+    void samplesToFloat(const uint8_t* in, size_t in_size, float* out);
+    void floatToSamples(const float* in, size_t in_size, uint8_t* out);
 
-    auto szDefaultOutputDevice = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
-    cout << "Default Output Device: " << szDefaultOutputDevice << endl;
+protected:
+    static constexpr ALCuint m_samplingRate = 44100;
+    static constexpr ALCsizei m_captureBufferSize = m_samplingRate / 2;
 
-    ALCdevice* audioDevice = alcOpenDevice(nullptr);
-    if (! audioDevice) {
-        errorCode = alcGetError(audioDevice);
-        cerr << "alcOpenDevice: " << errorCode << endl;
-        return 1;
-    }
+    ALCdevice* m_outputDevice;
+    ALCdevice* m_inputDevice;
+    ALCcontext* m_context;
+    list<ALuint> m_bufferQueue;
+    array<ALuint, 16> m_buffers;
+    ALuint m_source;
+    array<uint8_t, 2048> m_inBuffer;
+    array<uint8_t, 2048> m_outBuffer;
+};
 
-    ALCcontext* audioContext = alcCreateContext(audioDevice, nullptr);
-    if (! audioContext) {
-        errorCode = alcGetError(audioDevice);
-        cerr << "alcCreateContext: " << errorCode << endl;
-        return 1;
-    }
+AudioFrontend::AudioFrontend(void)
+{
+    m_outputDevice = alcOpenDevice(nullptr);
+    m_context = alcCreateContext(m_outputDevice, nullptr);
+    alcMakeContextCurrent(m_context);
 
-    alcMakeContextCurrent(audioContext);
+    alGenBuffers(m_buffers.size(), m_buffers.data());
+    alGenSources(1, &m_source);
 
-    alGetError();
-    alGenBuffers(helloBuffer.size(), helloBuffer.data());
-    errorCode = alGetError();
-    //cerr << "alGenBuffers: " << errorCode << endl;
+    m_bufferQueue.insert(end(m_bufferQueue), begin(m_buffers), end(m_buffers));
 
-    for (size_t i = 0; i < helloBuffer.size(); i++) {
-        //cout << "Pushing buffer " << helloBuffer[i] << endl;
-        bufferQueue.push_back(helloBuffer[i]);
-    }
+    m_inputDevice = alcCaptureOpenDevice(nullptr, m_samplingRate, AL_FORMAT_MONO8, m_captureBufferSize);
+    alcCaptureStart(m_inputDevice);
+}
 
-    alGetError();
-    alGenSources(1, &helloSource);
-    errorCode = alGetError();
-    //cerr << "alGenSources: " << errorCode << endl;
+AudioFrontend::~AudioFrontend(void)
+{
+    alcCaptureStop(m_inputDevice);
+    alcCaptureCloseDevice(m_inputDevice);
 
-    const ALCuint samplingRate = 22050;
-    const ALCsizei bufferSize = samplingRate / 2;
-    ALCdevice* inputDevice = alcCaptureOpenDevice(nullptr, samplingRate, AL_FORMAT_MONO16, bufferSize);
+    alSourceStopv(1, &m_source);
+    alSourcei(m_source, AL_BUFFER, 0);
 
-    if (! inputDevice) {
-        errorCode = alcGetError(inputDevice);
-        cerr << "alcCaptureOpenDevice: " << errorCode << endl;
-        return 1;
-    }
-
-    alcCaptureStart(inputDevice);
-    errorCode = alcGetError(inputDevice);
-    //cerr << "alcCaptureStart: " << errorCode << endl;
-
-    const ALCint captureSize = 2048;
-    uint16_t buffer[captureSize];
-    ALCint samplesIn = 0;
-    ALint availBuffers = 0;
-    ALuint myBuffer;
-    ALuint bufferHolder[16];
-    bool done = false;
-
-    while (not done) {
-        alGetSourcei(helloSource, AL_BUFFERS_PROCESSED, &availBuffers);
-
-        if (availBuffers > 0) {
-            //cout << "Available buffers: " << availBuffers << endl;
-            alSourceUnqueueBuffers(helloSource, availBuffers, &bufferHolder[0]);
-            for (int i = 0; i < availBuffers; i++) {
-                //cout << "< Push buffer " << bufferHolder[i] << endl;
-                bufferQueue.push_back(bufferHolder[i]);
-            }
-        }
-
-        alcGetIntegerv(inputDevice, ALC_CAPTURE_SAMPLES, 1, &samplesIn);
-
-        if (samplesIn < captureSize) {
-            continue;
-        }
-
-        //cout << samplesIn << endl;
-        alcCaptureSamples(inputDevice, buffer, captureSize);
-        errorCode = alcGetError(inputDevice);
-
-        if (not bufferQueue.empty()) {
-            myBuffer = bufferQueue.front();
-            bufferQueue.pop_front();
-            //cout << "> Pop buffer " << myBuffer << endl;
-
-            alBufferData(myBuffer, AL_FORMAT_MONO16, buffer, captureSize * sizeof(uint16_t), samplingRate);
-            errorCode = alGetError();
-            //cerr << "alBufferData: " << errorCode << endl;
-
-            alSourceQueueBuffers(helloSource, 1, &myBuffer);
-                ALint queuedBuffers;
-                alGetSourcei(helloSource, AL_BUFFERS_QUEUED, &queuedBuffers);
-                //cout << queuedBuffers << " buffers queued" << endl;
-
-            ALint sourceState = 0;
-            alGetSourcei(helloSource, AL_SOURCE_STATE, &sourceState);
-            if (sourceState != AL_PLAYING) {
-                alSourcePlay(helloSource);
-            }
-        }
-    }
-
-    alcCaptureStop(inputDevice);
-    alcCaptureCloseDevice(inputDevice);
-
-    alSourceStopv(1, &helloSource);
-    alSourcei(helloSource, AL_BUFFER, 0);
-
-    alDeleteSources(1, &helloSource);
-    alDeleteBuffers(helloBuffer.size(), helloBuffer.data());
-    errorCode = alGetError();
+    alDeleteSources(1, &m_source);
+    alDeleteBuffers(m_buffers.size(), m_buffers.data());
 
     alcMakeContextCurrent(nullptr);
-    errorCode = alGetError();
+    alcDestroyContext(m_context);
+    alcCloseDevice(m_outputDevice);
+}
 
-    alcDestroyContext(audioContext);
-    alcCloseDevice(audioDevice);
+void AudioFrontend::samplesToFloat(const uint8_t* in, size_t in_size, float* out)
+{
+    for (size_t i = 0; i < in_size; i++) {
+        out[i] = static_cast<float>(in[i]) / 128.f - 1.f;
+    }
+}
+
+void AudioFrontend::floatToSamples(const float* in, size_t in_size, uint8_t* out)
+{
+    for (size_t i = 0; i < in_size; i++) {
+        out[i] = static_cast<uint8_t>(round((in[i] + 1.f) * 128.f));
+    }
+}
+
+void AudioFrontend::transmit(const float* out, size_t out_size)
+{
+    size_t sent = 0;
+
+    while (sent < out_size) {
+        size_t toSend = min(out_size - sent, m_outBuffer.size());
+        ALint availBuffers = 0;
+        alGetSourcei(m_source, AL_BUFFERS_PROCESSED, &availBuffers);
+
+        if (availBuffers > 0) {
+            array<ALuint, 16> bufferHolder;
+            alSourceUnqueueBuffers(m_source, availBuffers, bufferHolder.data());
+            m_bufferQueue.insert(end(m_bufferQueue), begin(bufferHolder), begin(bufferHolder) + availBuffers);
+        }
+
+        while (m_bufferQueue.empty()) {
+            this_thread::sleep_for(chrono::milliseconds(10));
+            alGetSourcei(m_source, AL_BUFFERS_PROCESSED, &availBuffers);
+
+            if (availBuffers > 0) {
+                array<ALuint, 16> bufferHolder;
+                alSourceUnqueueBuffers(m_source, availBuffers, bufferHolder.data());
+                m_bufferQueue.insert(end(m_bufferQueue), begin(bufferHolder), begin(bufferHolder) + availBuffers);
+            }
+        }
+
+        ALuint myBuffer = m_bufferQueue.front();
+        m_bufferQueue.pop_front();
+
+        this->floatToSamples(out + sent, toSend, m_outBuffer.data());
+        alBufferData(myBuffer, AL_FORMAT_MONO8, m_outBuffer.data(), toSend * sizeof(uint8_t), m_samplingRate);
+
+        alSourceQueueBuffers(m_source, 1, &myBuffer);
+
+        ALint sourceState = 0;
+        alGetSourcei(m_source, AL_SOURCE_STATE, &sourceState);
+
+        if (sourceState != AL_PLAYING) {
+            alSourcePlay(m_source);
+        }
+
+        sent += toSend;
+    }
+}
+
+size_t AudioFrontend::receive(float* in, size_t in_size)
+{
+    size_t retrieved = min(in_size, m_inBuffer.size());
+    ALCint avail = 0;
+    alcGetIntegerv(m_inputDevice, ALC_CAPTURE_SAMPLES, 1, &avail);
+
+    do {
+        this_thread::sleep_for(chrono::milliseconds(10));
+        alcGetIntegerv(m_inputDevice, ALC_CAPTURE_SAMPLES, 1, &avail);
+    }
+    while (avail < static_cast<int>(retrieved));
+
+    alcCaptureSamples(m_inputDevice, m_inBuffer.data(), retrieved);
+    this->samplesToFloat(m_inBuffer.data(), retrieved, in);
+
+    return retrieved;
+}
+
+int main(int argc, char* argv[])
+{
+    AudioFrontend* audio = new AudioFrontend();
+    array<float, 2048> buffer;
+
+    while (true) {
+        size_t received = audio->receive(buffer.data(), buffer.size());
+        audio->transmit(buffer.data(), received);
+    }
+
+    delete audio;
 
     return 0;
 }

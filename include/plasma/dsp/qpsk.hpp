6f351536054b34051ca8c64521b59aa6f06db7db
Jean-Sébastien Dominique <jd@amateurradio.engineer>
2020-03-02 01:44:36 -0500

    Implemented QPSK block modulation/demodulation

--- include/plasma/dsp/qpsk.hpp
+++ include/plasma/dsp/qpsk.hpp
@@ -12,8 +12,11 @@ public:
     qpsk(void) = default;
     ~qpsk(void) = default;
 
+    vector<complex<double>> modulate(string const& in);
     complex<double> modulate(unsigned int symbol);
+
     unsigned int demodulate(complex<double> sample);
+    string demodulate(vector<complex<double>> const& in);
 };
 
 } // namespace
